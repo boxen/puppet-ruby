ce26c5c7934dd3721cade88c1dc61bce82b1e45f
Will Farrington <wfarr@github.com>
2012-10-11 16:56:58 -1000

    Add docs for 1-8-7 and 1-8-7-p358

--- manifests/1-8-7-p358.pp
+++ manifests/1-8-7-p358.pp
@@ -1,3 +1,9 @@
+# Installs ruby 1.8.7p358 from rbenv.
+# On 10.8 disables tk, tcl, and tcltk.
+#
+# Usage:
+#
+#     include ruby::1-8-7-p358
 class ruby::1-8-7-p358 {
   if $::macosx_productversion_major == 10.8 {
     $opts = '--disable-tk --disable-tcl --disable-tcltk-framework'
--- manifests/1-8-7.pp
+++ manifests/1-8-7.pp
@@ -1,3 +1,8 @@
+# Installs ruby 1.8.7p358 from rbenv and symlinks it as 1.8.7.
+#
+# Usage:
+#
+#     include ruby::1-8-7
 class ruby::1-8-7 {
   require rbenv
   require ruby::1-8-7-p358
