626b2a29e98edbfd53f7862bf3fb2c3178e73f2a
Will Farrington <wfarr@github.com>
2012-12-29 20:48:14 -0800

    missed a spot

--- manifests/1-9-3.pp
+++ manifests/1-9-3.pp
@@ -1,15 +1,15 @@
-# Installs ruby 1.9.3-p286 from rbenv and symlinks it as 1.9.3.
+# Installs ruby 1.9.3-p362 from rbenv and symlinks it as 1.9.3.
 #
 # Usage:
 #
 #     include ruby::1-9-3
 class ruby::1-9-3 {
   require rbenv
-  require ruby::1-9-3-p286
+  require ruby::1-9-3-p362
 
   file { "${rbenv::root}/versions/1.9.3":
     ensure  => symlink,
     force   => true,
-    target  => "${rbenv::root}/versions/1.9.3-p286"
+    target  => "${rbenv::root}/versions/1.9.3-p362"
   }
 }
