9ba9cd93e8ced30ebacf29baf39b11177aaf8dc2
Will Farrington <wfarr@github.com>
2013-01-08 19:41:05 -0800

    remove definitions

--- manifests/definition.pp
+++
@@ -1,9 +0,0 @@
-# A single ruby version definition
-
-define ruby::definition() {
-  file { "${homebrew::dir}/share/ruby-build/${name}":
-    source    => "puppet:///modules/ruby/definitions/${name}",
-    owner     => $::luser,
-    subscribe => Package['ruby-build']
-  }
-}
--- manifests/definitions.pp
+++
@@ -1,7 +0,0 @@
-# Custom ruby-build definitions for custom ruby versions
-
-class ruby::definitions {
-  #ruby::definition { ls("${settings::modulepath}/ruby/files/definitions", 1): ;}
-
-  Ruby::Definition <| |> -> Ruby <| |>
-}
