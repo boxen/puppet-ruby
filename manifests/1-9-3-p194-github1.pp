3e936abb149ad24ab3172562eaa53a680e830811
Will Farrington <wfarr@github.com>
2013-02-06 15:53:26 -0800

    doc typo

--- manifests/1-9-3-p194-github1.pp
+++ manifests/1-9-3-p194-github1.pp
@@ -2,7 +2,8 @@
 #
 # Usage:
 #
-#     include ruby::1-9-3-p194
+#     include ruby::1-9-3-p194-github1
+
 class ruby::1-9-3-p194-github1 {
   ruby::definition { '1.9.3-p194-github1': }
   ruby::version { '1.9.3-p194-github1': }
