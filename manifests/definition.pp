40cbc8a0cac8abb81a6359e53c6c3df919e0139e
Will Farrington <wfarr@github.com>
2013-02-06 17:01:09 -0800

    specs and docs

--- manifests/definition.pp
+++ manifests/definition.pp
@@ -1,3 +1,9 @@
+# Public: ruby::definition allows you to install a ruby-build definition.
+#
+#   source =>
+#     The puppet:// source to install from. If undef, looks in
+#     puppet:///modules/ruby/definitions/${name}.
+
 define ruby::definition($source = undef) {
   include homebrew
   include ruby
---
+++ spec/defines/ruby_definition_spec.rb
@@ -0,0 +1,48 @@
+require 'spec_helper'
+
+describe 'ruby::definition' do
+  let(:facts) do
+    {
+      :boxen_home => '/opt/boxen'
+    }
+  end
+
+  let(:title) { '1.9.3-p231-github1' }
+
+  let(:definition_path) do
+    [
+      '/opt',
+      'boxen',
+      'homebrew',
+      'Cellar',
+      'ruby-build',
+      '20130118',
+      'share',
+      'ruby-build',
+      title
+    ].join('/')
+  end
+
+  context "with source" do
+    let(:whatever_source) { 'puppet:///modules/ruby/whatever_def' }
+    let(:params) do
+      {
+        :source => whatever_source
+      }
+    end
+
+    it do
+      should contain_file(definition_path).with_source(whatever_source)
+    end
+  end
+
+  it do
+    should include_class('homebrew')
+    should include_class('ruby')
+
+    should contain_file(definition_path).with({
+      :source  => "puppet:///modules/ruby/definitions/#{title}",
+      :require => 'Package[ruby-build]'
+    })
+  end
+end
