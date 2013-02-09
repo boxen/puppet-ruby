5302c72b833df1ea003a23355a1a721b5920beac
Will Farrington <wfarr@github.com>
2013-02-09 15:34:20 -0800

    add 1.9.3-p231-tcs-github, new default global ruby and 1.9.3

---
+++ manifests/1-9-3-p231-tcs-github.pp
@@ -0,0 +1,13 @@
+# Installs ruby 1.9.3-p231-tcs-github from rbenv.
+# Default global ruby version.
+#
+# Usage:
+#
+#     include ruby::1-9-3-p231-tcs-github
+
+class ruby::1-9-3-p231-github1 {
+  ruby::definition { '1.9.3-p231-tcs-github': }
+  ruby::version    { '1.9.3-p231-tcs-github':
+    global => true
+  }
+}
--- manifests/1-9-3.pp
+++ manifests/1-9-3.pp
@@ -1,15 +1,15 @@
-# Installs ruby 1.9.3-p231-github1 from rbenv and symlinks it as 1.9.3.
+# Installs ruby 1.9.3-p231-tcs-github from rbenv and symlinks it as 1.9.3.
 #
 # Usage:
 #
 #     include ruby::1-9-3
 class ruby::1-9-3 {
   require ruby
-  require ruby::1-9-3-p231-github1
+  require ruby::1-9-3-p231-tcs-github
 
   file { "${ruby::root}/versions/1.9.3":
     ensure  => symlink,
     force   => true,
-    target  => "${ruby::root}/versions/1.9.3-p231-github1"
+    target  => "${ruby::root}/versions/1.9.3-p231-tcs-github"
   }
 }
