86786f5b15391de167c4df2b3c77bcc1a39ff369
Will Farrington <wfarr@github.com>
2013-02-23 15:14:57 +1100

    change default public 1.9.3 to use 1.9.3-p385

--- manifests/1-9-3.pp
+++ manifests/1-9-3.pp
@@ -1,15 +1,15 @@
-# Installs ruby 1.9.3-p231-tcs-github1 from rbenv and symlinks it as 1.9.3.
+# Installs ruby 1.9.3-p385 from rbenv and symlinks it as 1.9.3.
 #
 # Usage:
 #
 #     include ruby::1-9-3
 class ruby::1-9-3 {
   require ruby
-  require ruby::1-9-3-p231-tcs-github1
+  require ruby::1-9-3-p385
 
   file { "${ruby::root}/versions/1.9.3":
     ensure  => symlink,
     force   => true,
-    target  => "${ruby::root}/versions/1.9.3-p231-tcs-github1"
+    target  => "${ruby::root}/versions/1.9.3-p385"
   }
 }
