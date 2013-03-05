c6e8088c558357c4c3365a4019f82a478da77aef
Will Farrington <wfarr@github.com>
2013-03-04 19:44:00 -0800

    Fix specs

--- spec/defines/ruby_definition_spec.rb
+++ spec/defines/ruby_definition_spec.rb
@@ -41,7 +41,7 @@ describe 'ruby::definition' do
 
     should contain_file(definition_path).with({
       :source  => "puppet:///modules/ruby/definitions/#{title}",
-      :require => 'Exec[ensure-ruby-build-version-v20130224]'
+      :require => 'Exec[ensure-ruby-build-version-v20130227]'
     })
   end
 end
