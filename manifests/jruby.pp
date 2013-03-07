d9ca59586412e6b9cc48e8885c18e0a6f2a3872a
Will Farrington <wfarr@github.com>
2013-03-07 14:20:55 -0500

    DOC IT

--- manifests/jruby.pp
+++ manifests/jruby.pp
@@ -1,3 +1,10 @@
+# Public: Installs our blessed version of jruby and symlinks it in rbenv
+# to "jruby"
+#
+# Usage:
+#
+#   include ruby::jruby
+
 class ruby::jruby {
   require ruby
   require ruby::jruby_1_7_3
--- manifests/jruby_1_7_3.pp
+++ manifests/jruby_1_7_3.pp
@@ -1,3 +1,10 @@
+# Public: Installs JRuby 1.7.3 from rbenv
+#
+# Usage:
+#
+#   include ruby::jruby_1_7_3
+
+
 class ruby::jruby_1_7_3 {
   include ruby
   include java
