d8742cd3d4d21b5edc51dd2400991966b45c30fe
Will Farrington <wfarr@github.com>
2013-03-08 17:50:31 -0500

    1.9.3-p231-tcs-github-1.0.11

---
+++ manifests/1_9_3_p231_tcs_github_1_0_11.pp
@@ -0,0 +1,10 @@
+# Installs ruby 1.9.3-p231-tcs-github-1.0.11 from rbenv.
+#
+# Usage:
+#
+#     include ruby::1.9.3-p231-tcs-github-1.0.11
+
+class ruby::1_9_3_p231_tcs_github_1_0_11 {
+  ruby::definition { '1.9.3-p231-tcs-github-1.0.11': }
+  ruby::version    { '1.9.3-p231-tcs-github-1.0.11': }
+}
