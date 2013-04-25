ab746c295efbcd873ec6e1825f835da8836c6ef9
Will Farrington <wfarr@github.com>
2013-04-24 23:30:45 -0700

    Make ruby::gem take ensure and pass it to rbenv_gem

--- manifests/gem.pp
+++ manifests/gem.pp
@@ -7,10 +7,11 @@
 #       ruby    => '1.9.3-p194',
 #       version => '~> 1.2.0'
 #     }
-define ruby::gem($gem, $ruby, $version = '>= 0') {
+define ruby::gem($ensure = 'present', $gem, $ruby, $version = '>= 0') {
   require ruby
 
   rbenv_gem { $name:
+    ensure        => $ensure,
     gem           => $gem,
     version       => $version,
     rbenv_root    => $ruby::root,
