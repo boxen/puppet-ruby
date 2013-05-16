dc060ed04ee6235b03c79756198915bb13514941
Will Farrington <wfarr@github.com>
2013-05-16 12:42:58 -0400

    Add some more specs

--- spec/classes/ruby_spec.rb
+++ spec/classes/ruby_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe 'ruby' do
   let(:facts) { default_test_facts }
 
-  let(:params) do
+  let(:default_params) do
     {
       :default_gems  => [],
       :rbenv_plugins => {},
@@ -11,6 +11,8 @@ describe 'ruby' do
       :root          => '/test/boxen/rbenv',
     }
   end
+
+  let(:params) { default_params }
 
   it do
     should include_class("ruby::params")
@@ -37,6 +39,38 @@ describe 'ruby' do
 
       should contain_file('/test/boxen/env.d/rbenv.sh').
         with_source('puppet:///modules/ruby/rbenv.sh')
+    end
+  end
+
+  context "default_gems" do
+    let(:params) do
+      default_params.merge(:default_gems => ["bundler ~>1.3", "pry"])
+    end
+
+    it do
+      should contain_file("/test/boxen/rbenv/default-gems").with({
+        :content => "\n  bundler ~>1.3\n\n  pry\n\n"
+      })
+    end
+  end
+
+  context "rbenv plugins" do
+    let(:params) do
+      default_params.merge(
+        :rbenv_plugins => {
+          'ruby-build' => {
+            'ensure' => 'v20130514',
+            'source' => 'sstephenson/ruby-build'
+          }
+        }
+      )
+    end
+
+    it do
+      should contain_ruby__plugin('ruby-build').with({
+        :ensure => 'v20130514',
+        :source => 'sstephenson/ruby-build'
+      })
     end
   end
 end
