80b6d80bb544b8909f15e7feaa9c043c452cdb8e
Will Farrington <wfarr@github.com>
2013-06-14 17:38:11 -0700

    Set CC to gcc-48 when compiling 1.8.7 releases for Mavericks

--- manifests/1_8_7_p358.pp
+++ manifests/1_8_7_p358.pp
@@ -15,12 +15,23 @@ class ruby::1_8_7_p358 {
         'CC' => "${boxen::config::home}/homebrew/bin/gcc-4.2"
       }
 
-      if $::macosx_productversion_major == '10.8' {
-        $env = merge($default_env, {
-          'CONFIGURE_OPTS' => '--disable-tk --disable-tcl --disable-tcltk-framework'
-        })
-      } else {
-        $env = $default_env
+      case $::macosx_productversion_major {
+        '10.8': {
+          $env = merge($default_env, {
+            'CONFIGURE_OPTS' => '--disable-tk --disable-tcl --disable-tcltk-framework'
+          })
+        }
+
+        '10.9': {
+          $env = merge($default_env, {
+            'CONFIGURE_OPTS' => '--disable-tk --disable-tcl --disable-tcltk-framework',
+            'CC'             => 'gcc-48'
+          })
+        }
+
+        default: {
+          $env = $default_env
+        }
       }
     }
 
