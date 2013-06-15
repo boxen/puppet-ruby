1db00899da1fc1c6088879b4253f77409d9f3c53
Will Farrington <wfarr@github.com>
2013-06-14 18:20:52 -0700

    Under Mavericks, Ruby needs a bit of help finding the X11 headers.

--- manifests/1_9_3_p231_tcs_github_1_0_30.pp
+++ manifests/1_9_3_p231_tcs_github_1_0_30.pp
@@ -5,8 +5,15 @@
 #     include ruby::1.9.3-p231-tcs-github-1.0.30
 
 class ruby::1_9_3_p231_tcs_github_1_0_30 {
-  include autoconf
+  require autoconf
+  require xquartz
 
   ruby::definition { '1.9.3-p231-tcs-github-1.0.30': }
   ruby::version    { '1.9.3-p231-tcs-github-1.0.30': }
+
+  if $::operatingsystem == 'Darwin' and $::macosx_productversion_major == '10.9' {
+    Ruby::Version['1.9.3-p231-tcs-github-1.0.30'] {
+      env => { 'CFLAGS' => '-I/opt/X11/include' }
+    }
+  }
 }
