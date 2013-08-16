865d22b09e098d1ed821008d11e3329aa7c9d7f0
David Goodlad <david@goodlad.net>
2013-08-16 14:33:15 -0700

    Replace rbenv with a simple chruby install
    Adds chruby and ruby-build in /opt/boxen/{chruby,ruby-build}

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,61 +1,49 @@
 # Class: ruby
 #
-# This module installs a full rbenv-driven ruby stack
+# This module installs a full chruby-driven ruby stack
 #
 class ruby(
-  $default_gems  = $ruby::params::default_gems,
-  $rbenv_plugins = {},
-  $rbenv_version = $ruby::params::rbenv_version,
-  $rbenv_root    = $ruby::params::rbenv_root,
-  $user          = $ruby::params::user
+  $default_gems      = $ruby::params::default_gems,
+  $chruby_version    = $ruby::params::chruby_version,
+  $chruby_root       = $ruby::params::chruby_root,
+  $rubybuild_version = $ruby::params::rubybuild_version
+  $rubybuild_root    = $ruby::params::rubybuild_root
+  $user              = $ruby::params::user
 ) inherits ruby::params {
 
   if $::osfamily == 'Darwin' {
     include boxen::config
 
-    file { "${boxen::config::envdir}/rbenv.sh":
-      source => 'puppet:///modules/ruby/rbenv.sh' ;
+    file { "${boxen::config::envdir}/chruby.sh":
+      source => 'puppet:///modules/ruby/chruby.sh' ;
     }
   }
 
-  repository { $rbenv_root:
-    ensure => $rbenv_version,
-    source => 'sstephenson/rbenv',
-    user   => $user
+  repository { $chruby_root:
+    ensure => $chruby_version,
+    source => 'postmodern/chruby',
+    user   => $user,
   }
 
-  file {
-    [
-      "${rbenv_root}/plugins",
-      "${rbenv_root}/rbenv.d",
-      "${rbenv_root}/rbenv.d/install",
-      "${rbenv_root}/shims",
-      "${rbenv_root}/versions",
-    ]:
-      ensure  => directory,
-      require => Repository[$rbenv_root];
-
-    "${rbenv_root}/rbenv.d/install/00_try_to_download_ruby_version.bash":
-      mode   => '0755',
-      source => 'puppet:///modules/ruby/try_to_download_ruby_version.bash';
+  repository { $rubybuild_root:
+    ensure => $rubybuild_version,
+    source => 'sstephenson/ruby-build',
+    user   => $user,
   }
 
-  $_real_rbenv_plugins = merge($ruby::params::rbenv_plugins, $rbenv_plugins)
-  create_resources('ruby::plugin', $_real_rbenv_plugins)
+  #file {
+  #  [
+  #    "${rbenv_root}/versions",
+  #  ]:
+  #    ensure  => directory,
+  #    require => Repository[$rbenv_root];
 
+  #  "${rbenv_root}/rbenv.d/install/00_try_to_download_ruby_version.bash":
+  #    mode   => '0755',
+  #    source => 'puppet:///modules/ruby/try_to_download_ruby_version.bash';
+  #}
 
-  if has_key($_real_rbenv_plugins, 'rbenv-default-gems') {
-    $gem_list = join($default_gems, "\n")
-
-    file { "${rbenv_root}/default-gems":
-      content => "${gem_list}\n",
-      tag     => 'ruby_plugin_config'
-    }
-  }
-
-  Repository[$rbenv_root] ->
-    File <| tag == 'ruby_plugin_config' |> ->
-    Ruby::Plugin <| |> ->
-    Ruby::Definition <| |> ->
-    Ruby::Version <| |>
+  #Repository[$chruby_root] ->
+  #  Ruby::Definition <| |> ->
+  #  Ruby::Version <| |>
 }
--- manifests/params.pp
+++ manifests/params.pp
@@ -4,32 +4,20 @@ class ruby::params {
     'Darwin': {
       include boxen::config
 
-      $rbenv_root = "${boxen::config::home}/rbenv"
-      $user       = $::boxen_user
+      $chruby_root    = "${boxen::config::home}/chruby"
+      $rubybuild_root = "${boxen::config::home}/ruby-build"
+      $user           = $::boxen_user
     }
 
     default: {
-      $rbenv_root = '/usr/local/share/rbenv'
-      $user       = 'root'
+      $chruby_root    = '/usr/local/share/chruby'
+      $rubybuild_root = '/usr/local/share/ruby-build'
+      $user           = 'root'
     }
   }
 
-  $rbenv_version = 'v0.4.0'
+  $chruby_version    = 'v0.3.6'
+  $rubybuild_version = 'v20130806'
 
   $default_gems = ['bundler ~>1.3']
-
-  $rbenv_plugins = {
-    'ruby-build' => {
-      'ensure' => 'v20130628',
-      'source' => 'sstephenson/ruby-build'
-    },
-    'rbenv-gem-rehash' => {
-      'ensure' => 'v1.0.0',
-      'source' => 'sstephenson/rbenv-gem-rehash'
-    },
-    'rbenv-default-gems' => {
-      'ensure' => 'v1.0.0',
-      'source' => 'sstephenson/rbenv-default-gems'
-    }
-  }
 }
