e649515487e13e0171bd0f555a34a0f12dafc16e
David Goodlad <david@goodlad.net>
2013-08-16 14:40:16 -0700

    Setup env.d script for chruby

--- files/rbenv.sh
+++
@@ -1,7 +0,0 @@
-# Configure and activate rbenv. You know, for rubies.
-
-export RBENV_ROOT=$BOXEN_HOME/rbenv
-
-export PATH=$BOXEN_HOME/rbenv/bin:$BOXEN_HOME/rbenv/plugins/ruby-build/bin:$PATH
-
-eval "$(rbenv init -)"
