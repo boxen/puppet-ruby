ba77743674ae7ac1e57029222093844a44482edb
David Goodlad <david@goodlad.net>
2013-08-16 18:30:37 -0700

    @wfarr was thinking ahead

--- files/chruby-install.sh
+++ files/chruby-install.sh
@@ -10,7 +10,7 @@ if [ -z "$SKIP_PRECOMPILED_RUBIES" ]; then
 
   cd $VERSIONS_DIR
   echo "Trying to download precompiled Ruby from Boxen..."
-  curl -s -f http://s3.amazonaws.com/boxen-downloads/rbenv/$OSX_RELEASE/$VERSION_NAME.tar.bz2 > /tmp/ruby-$VERSION_NAME.tar.bz2 && \
+  curl -s -f http://s3.amazonaws.com/boxen-downloads/chruby/$OSX_RELEASE/$VERSION_NAME.tar.bz2 > /tmp/ruby-$VERSION_NAME.tar.bz2 && \
     tar xjf /tmp/ruby-$VERSION_NAME.tar.bz2 && rm -rf /tmp/ruby-$VERSION_NAME.tar.bz2
   cd $PREV_PWD
 
