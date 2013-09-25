4e6f28925890c55619df6b95c950458c6858a81f
Charlie Somerville <charlie@charliesomerville.com>
2013-09-25 21:27:03 +1000

    add 2.0.0-github6

---
+++ manifests/2_0_0_github6.pp
@@ -0,0 +1,13 @@
+# Installs ruby 2.0.0-github6 from rbenv.
+#
+# Usage:
+#
+#     include ruby::2_0_0_github6
+
+class ruby::2_0_0_github6 {
+  require autoconf
+  require xquartz
+
+  ruby::definition { '2.0.0-github6': }
+  ruby::version    { '2.0.0-github6': }
+}
