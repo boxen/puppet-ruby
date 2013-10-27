c545a009088c7aa237f255480186e8175e6993b5
Rafael Mendonça França <rafael.franca@plataformatec.com.br>
2013-10-27 18:25:16 -0200

    Use the same environment variables used on puppet-homebrew
    See
    https://github.com/boxen/puppet-homebrew/blob/2a5183acff6b9ee8a476c14b7bfc3361fdd5daaa/files/boxen-monkeypatches.rb#L17-18

--- files/try_to_download_ruby_version.bash
+++ files/try_to_download_ruby_version.bash
@@ -4,12 +4,13 @@ if [ -z "$SKIP_PRECOMPILED_RUBIES" ]; then
   VERSION_NAME="${DEFINITION##*/}"
   VERSIONS_DIR="${RBENV_ROOT}/versions"
   OSX_RELEASE=`sw_vers -productVersion | cut -f 1-2 -d '.'`
-  DOWNLOAD_BUCKET=${BOXEN_RUBIES_BUCKET-"boxen-downloads"}
+  DOWNLOAD_BUCKET=${BOXEN_S3_BUCKET-"boxen-downloads"}
+  DOWNLOAD_HOST=${BOXEN_S3_HOST-"s3.amazonaws.com"}
   PREV_PWD=`pwd`
 
   cd $VERSIONS_DIR
   echo "Trying to download precompiled Ruby from Boxen..."
-  curl -s -f http://s3.amazonaws.com/$DOWNLOAD_BUCKET/rbenv/$OSX_RELEASE/$VERSION_NAME.tar.bz2 > /tmp/ruby-$VERSION_NAME.tar.bz2 && \
+  curl -s -f http://$DOWNLOAD_HOST/$DOWNLOAD_BUCKET/rbenv/$OSX_RELEASE/$VERSION_NAME.tar.bz2 > /tmp/ruby-$VERSION_NAME.tar.bz2 && \
     tar xjf /tmp/ruby-$VERSION_NAME.tar.bz2 && rm -rf /tmp/ruby-$VERSION_NAME.tar.bz2
   cd $PREV_PWD
 
