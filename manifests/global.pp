e93bdefd1033bc1235430fb4f7613ecf90462a6f
Will Farrington <wfarr@github.com>
2013-10-29 16:59:31 -0700

    Fix ruby::global

--- manifests/global.pp
+++ manifests/global.pp
@@ -5,17 +5,16 @@
 #   class { 'ruby::global': version => '1.9.3' }
 
 class ruby::global($version = '1.9.3') {
-  #include ruby
+  require ruby
 
-  #if $version != 'system' {
-  #  $klass = join(['ruby', join(split($version, '[.-]'), '_')], '::')
-  #  require $klass
-  #}
+  if $version != 'system' {
+   $klass = join(['ruby', join(split($version, '[.-]'), '_')], '::')
+   require $klass
+  }
 
-  #file { "${ruby::rbenv_root}/version":
-  #  ensure  => present,
-  #  owner   => $ruby::user,
-  #  mode    => '0644',
-  #  content => "${version}\n",
-  #}
+  boxen::env_script { 'default_ruby_version':
+    ensure   => present,
+    content  => "chruby ${version}\n",
+    priority => 'lowest',
+  }
 }
