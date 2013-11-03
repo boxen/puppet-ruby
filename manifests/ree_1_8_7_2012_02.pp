6fd0c54c03d5342d548c97f349360d0c76ab5548
Charlie Somerville <charlie@charliesomerville.com>
2013-11-02 17:18:56 -0700

    Fix compilation of REE on 10.8

--- manifests/ree_1_8_7_2012_02.pp
+++ manifests/ree_1_8_7_2012_02.pp
@@ -13,12 +13,12 @@ class ruby::ree_1_8_7_2012_02 {
 
       $cc = $::macosx_productversion_major ? {
         '10.9'  => "${homebrew::config::installdir}/homebrew/bin/gcc-4.8",
-        default => "${homebrew::config::installdir}/homebrew/bin/gcc-4.2",
+        default => "${homebrew::config::installdir}/bin/gcc-4.2",
       }
 
       $env = {
         'CC'       => $cc,
-        'CPPFLAGS' => "'-I${homebrew::config::installdir}/include -I/opt/X11/include'",
+        'CPPFLAGS' => "-I${homebrew::config::installdir}/include -I/opt/X11/include",
       }
     }
 
