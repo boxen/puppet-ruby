13bb585aac39c1c02dd2d182025c625c64a5553d
Will Farrington <wfarr@github.com>
2013-11-08 10:25:37 -0800

    spike making the chruby-install script work across platforms

--- files/chruby-install.sh
+++ files/chruby-install.sh
@@ -4,16 +4,29 @@ VERSION_NAME=$1
 
 if [ -z "$SKIP_PRECOMPILED_RUBIES" ]; then
   VERSIONS_DIR="${CHRUBY_ROOT}/versions"
-  OSX_RELEASE=`sw_vers -productVersion | cut -f 1-2 -d '.'`
+
+  PLATFORM="$(/usr/bin/uname -s | /usr/bin/tr '[:upper:]' '[:lower:]')"
+
+  if [ "$PLATFORM" = "darwin" ]; then
+    OS_RELEASE=`/usr/bin/sw_vers -productVersion | /usr/bin/cut -f 1-2 -d '.'`
+  else
+    if [ -x "/usr/bin/lsb_release" ]; then
+      OS_RELEASE="$(/usr/bin/lsb_release --codename --short)"
+    fi
+  fi
+
+  if [ -z "$OS_RELEASE" ]; then
+    echo "There are no precompiled binaries for this operating system" >2
+    return
+  fi
+
   DOWNLOAD_BUCKET=${BOXEN_S3_BUCKET-"boxen-downloads"}
   DOWNLOAD_HOST=${BOXEN_S3_HOST-"s3.amazonaws.com"}
   PREV_PWD=`pwd`
 
-  cd $VERSIONS_DIR
-  echo "Trying to download precompiled Ruby from Boxen..."
-  curl -s -f http://$DOWNLOAD_HOST/$DOWNLOAD_BUCKET/chruby/$OSX_RELEASE/$VERSION_NAME.tar.bz2 > /tmp/ruby-$VERSION_NAME.tar.bz2 && \
-    tar xjf /tmp/ruby-$VERSION_NAME.tar.bz2 && rm -rf /tmp/ruby-$VERSION_NAME.tar.bz2
-  cd $PREV_PWD
+  echo "Trying to download precompiled Ruby from S3..."
+  curl --silent --fail http://$DOWNLOAD_HOST/$DOWNLOAD_BUCKET/chruby/$PLATFORM/$OS_RELEASE/$VERSION_NAME.tar.bz2 \
+    | tar -C $VERSIONS_DIR jxf
 
   if [ -d "$VERSIONS_DIR/$VERSION_NAME" ]; then
     # install from download was successful
