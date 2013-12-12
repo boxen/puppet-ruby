a6216a1659f64cd272e6be128cb525b2f8d6d642
David Goodlad <david@goodlad.net>
2013-12-12 11:42:27 +1100

    Port 1.9.2-p290 to hiera

--- manifests/1_9_2_p290.pp
+++ manifests/1_9_2_p290.pp
@@ -6,23 +6,5 @@
 class ruby::1_9_2_p290 {
   require gcc
 
-  case $::osfamily {
-    Darwin: {
-      include boxen::config
-
-      $env = {
-        'CC' => "${boxen::config::home}/homebrew/bin/gcc-4.2"
-      }
-    }
-
-    default: {
-      $env = {
-        'CC' => 'gcc'
-      }
-    }
-  }
-
-  ruby::version { '1.9.2-p290':
-    env => $env
-  }
+  ruby::version { '1.9.2-p290': }
 }
