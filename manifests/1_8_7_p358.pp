6e8ad92bc06a9f1ed47f31e861b473b3f34017dd
David Goodlad <david@goodlad.net>
2013-12-12 11:38:47 +1100

    Port 1.8.7-p358 to use hiera data

--- manifests/1_8_7_p358.pp
+++ manifests/1_8_7_p358.pp
@@ -7,42 +7,5 @@
 class ruby::1_8_7_p358 {
   require gcc
 
-  case $::osfamily {
-    Darwin: {
-      include boxen::config
-
-      $default_env = {
-        'CC' => "${boxen::config::home}/homebrew/bin/gcc-4.2"
-      }
-
-      case $::macosx_productversion_major {
-        '10.8': {
-          $env = merge($default_env, {
-            'CONFIGURE_OPTS' => '--disable-tk --disable-tcl --disable-tcltk-framework'
-          })
-        }
-
-        '10.9': {
-          $env = merge($default_env, {
-            'CONFIGURE_OPTS' => '--disable-tk --disable-tcl --disable-tcltk-framework',
-            'CC'             => 'gcc-48'
-          })
-        }
-
-        default: {
-          $env = $default_env
-        }
-      }
-    }
-
-    default: {
-      $env = {
-        'CC' => 'gcc'
-      }
-    }
-  }
-
-  ruby::version { '1.8.7-p358':
-    env => $env
-  }
+  ruby::version { '1.8.7-p358': }
 }
