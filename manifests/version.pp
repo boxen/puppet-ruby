067ec8a090365e3273743b277c2728ad56ca36aa
David Goodlad <david@goodlad.net>
2013-12-22 14:32:38 +1100

    Don't try to install aliases

--- manifests/version.pp
+++ manifests/version.pp
@@ -23,74 +23,73 @@ define ruby::version(
     }
 
     ensure_resource('ruby::version', $target)
-  }
-
-
-  case $::osfamily {
-    'Darwin': {
-      require xquartz
-      include homebrew::config
-      include boxen::config
-
-      $os_env = {
-        'BOXEN_S3_HOST'   => $::boxen_s3_host,
-        'BOXEN_S3_BUCKET' => $::boxen_s3_bucket,
-        'CFLAGS'          => "-I${homebrew::config::installdir}/include -I/opt/X11/include",
-        'LDFLAGS'         => "-L${homebrew::config::installdir}/lib -L/opt/X11/lib",
-      }
-    }
-
-    default: {
-      $os_env = {}
-    }
-  }
-
-  $dest = "${ruby::rbenv_root}/versions/${version}"
-
-  if $ensure == 'absent' {
-    file { $dest:
-      ensure => absent,
-      force  => true
-    }
   } else {
-    $default_env = {
-      'CC'         => '/usr/bin/cc',
-      'RBENV_ROOT' => $ruby::rbenv_root
-    }
+    case $::osfamily {
+      'Darwin': {
+        require xquartz
+        include homebrew::config
+        include boxen::config
 
-    $hierdata = hiera_hash('ruby::version::env', {})
-    if has_key($hierdata, $version) {
-      $hiera_env = $hierdata[$version]
-    } else {
-      $hiera_env = {}
-    }
+        $os_env = {
+          'BOXEN_S3_HOST'   => $::boxen_s3_host,
+          'BOXEN_S3_BUCKET' => $::boxen_s3_bucket,
+          'CFLAGS'          => "-I${homebrew::config::installdir}/include -I/opt/X11/include",
+          'LDFLAGS'         => "-L${homebrew::config::installdir}/lib -L/opt/X11/lib",
+        }
+      }
 
-    $final_env = merge(merge(merge($default_env, $os_env), $hiera_env), $env)
-
-    if has_key($final_env, 'CC') {
-      case $final_env['CC'] {
-        /gcc/:   { require gcc }
-        default: { }
+      default: {
+        $os_env = {}
       }
     }
 
-    exec { "ruby-install-${version}":
-      command     => "${ruby::rbenv_root}/bin/rbenv install ${version}",
-      cwd         => "${ruby::rbenv_root}/versions",
-      provider    => 'shell',
-      timeout     => 0,
-      creates     => $dest,
-      user        => $ruby::user,
-    }
-    ->
-    ruby::gem { "bundler for ${version}":
-      gem     => 'bundler',
-      ruby    => $version,
-      version => '~> 1.0'
-    }
+    $dest = "${ruby::rbenv_root}/versions/${version}"
 
-    Exec["ruby-install-${version}"] {
-      environment +> sort(join_keys_to_values($final_env, '='))
+    if $ensure == 'absent' {
+      file { $dest:
+        ensure => absent,
+        force  => true
+      }
+    } else {
+      $default_env = {
+        'CC'         => '/usr/bin/cc',
+        'RBENV_ROOT' => $ruby::rbenv_root
+      }
+
+      $hierdata = hiera_hash('ruby::version::env', {})
+      if has_key($hierdata, $version) {
+        $hiera_env = $hierdata[$version]
+      } else {
+        $hiera_env = {}
+      }
+
+      $final_env = merge(merge(merge($default_env, $os_env), $hiera_env), $env)
+
+      if has_key($final_env, 'CC') {
+        case $final_env['CC'] {
+          /gcc/:   { require gcc }
+          default: { }
+        }
+      }
+
+      exec { "ruby-install-${version}":
+        command     => "${ruby::rbenv_root}/bin/rbenv install ${version}",
+        cwd         => "${ruby::rbenv_root}/versions",
+        provider    => 'shell',
+        timeout     => 0,
+        creates     => $dest,
+        user        => $ruby::user,
+      }
+      ->
+      ruby::gem { "bundler for ${version}":
+        gem     => 'bundler',
+        ruby    => $version,
+        version => '~> 1.0'
+      }
+
+      Exec["ruby-install-${version}"] {
+        environment +> sort(join_keys_to_values($final_env, '='))
+      }
     }
   }
 }
