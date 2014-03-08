a01d74ad4e4f57cd8c10439d55940f7304e79983
Will Farrington <wfarr@github.com>
2014-03-08 19:21:24 +1300

    Move the OS-based ENV config to hiera

--- manifests/version.pp
+++ manifests/version.pp
@@ -27,43 +27,34 @@ define ruby::version(
       default: { }
     }
 
-    case $::osfamily {
-      'Darwin': {
-        require xquartz
-        include homebrew::config
-        include boxen::config
-
-        $os_env = {
-          'BOXEN_S3_HOST'   => $::boxen_s3_host,
-          'BOXEN_S3_BUCKET' => $::boxen_s3_bucket,
-          'CFLAGS'          => "-I${homebrew::config::installdir}/include -I/opt/X11/include",
-          'LDFLAGS'         => "-L${homebrew::config::installdir}/lib -L/opt/X11/lib",
-        }
-      }
-
-      default: {
-        $os_env = {}
-      }
-    }
-
     $default_env = {
       'CC' => '/usr/bin/cc',
     }
 
-    $hierdata = hiera_hash('ruby::version::env', {})
-    if has_key($hierdata, $version) {
-      $hiera_env = $hierdata[$version]
-    } else {
-      $hiera_env = {}
+    if $::operatingsystem == 'Darwin' {
+      require xquartz
+      include homebrew::config
+      include boxen::config
     }
 
-    $final_env = merge(merge(merge($default_env, $os_env), $hiera_env), $env)
+    $hierdata = hiera_hash('ruby::version::env', {})
 
-    if has_key($final_env, 'CC') {
-      case $final_env['CC'] {
-        /gcc/:   { require gcc }
-        default: { }
-      }
+    if has_key($hierdata, $::operatingsystem) {
+      $os_env = $hierdata[$::operatingsystem]
+    } else {
+      $os_env = {}
+    }
+
+    if has_key($hierdata, $version) {
+      $version_env = $hierdata[$version]
+    } else {
+      $version_env = {}
+    }
+
+    $_env = merge(merge(merge($default_env, $os_env), $version_env), $env)
+
+    if has_key($_env, 'CC') and $_env['CC'] =~ /gcc/ {
+      require gcc
     }
 
     ensure_resource('file', "${ruby::prefix}/rubies", {
@@ -73,7 +64,7 @@ define ruby::version(
 
     ruby { $version:
       ensure      => $ensure,
-      environment => $final_env,
+      environment => $_env,
       prefix      => "${ruby::prefix}/rubies/${version}",
       ruby_build  => "${ruby::prefix}/ruby-build/bin/ruby-build",
       user        => $ruby::user,
